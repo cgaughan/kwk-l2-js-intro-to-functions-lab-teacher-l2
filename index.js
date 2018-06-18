1edc5088dbd8fc7db0bab7931f72b9279ffb9b63
Caroline Gaughan <cgaughan1122@gmail.com>
2018-06-18 21:25:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code in this file!
+function getUKTime() {
+  currentHour = new Date().getHours() + 8
+  return currentHour
+}
+
+function discountTicket(original) {
+  reducePrice = original*.8;
+  return reducePrice;
+}
+
+function tweetReply(fanName) {
+  return `Hey ${fanName}, thanks so much for the support! I really appreciate it!`
+}
